dd9b2ca14ed79dd16396ced033ae8fcde6d5c28f
thujeevan <thujee@gmail.com>
2016-06-08 12:28:09 +0530

    implement configure store module

--- thujeevan/remo/store/index.js
+++ thujeevan/remo/store/index.js
@@ -1,1 +1,9 @@
-// TODO: configure store with api, thunk middlewares
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+
+import api from '../middleware/api';
+import rootReducer from '../reducers';
+
+export default function configureStore(preloadedState) {
+    return createStore(rootReducer, preloadedState, applyMiddleware(thunk, api));
+}
