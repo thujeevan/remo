1ff3e27e06aa318f7b5424d2f1f0d38b6eeb96bd
thujeevan <thujee@gmail.com>
2016-06-08 12:29:01 +0530

    initialized store

--- thujeevan/remo/index.js
+++ thujeevan/remo/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import {browserHistory} from 'react-router';
 
+import configureStore from './store';
 import Root from './containers/Root';
 
-render(<Root history={browserHistory}/>, document.getElementById('root'));
+const store = configureStore();
+
+render(<Root history={browserHistory} store={store}/>, document.getElementById('root'));
