232d97746ee7076775736e8d78413436ee6a385f
thujeevan <thujee@gmail.com>
2016-06-09 16:10:09 +0530

    add support for redux devtool chrome extension

--- thujeevan/remo/store/index.js
+++ thujeevan/remo/store/index.js
@@ -1,9 +1,12 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import api from '../middleware/api';
 import rootReducer from '../reducers';
 
 export default function configureStore(preloadedState) {
-    return createStore(rootReducer, preloadedState, applyMiddleware(thunk, api));
+    return createStore(rootReducer, preloadedState, compose(
+        applyMiddleware(thunk, api),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
 }
