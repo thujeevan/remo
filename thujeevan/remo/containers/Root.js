8c0cf62cdfd0a9c0ef70076bb7480f977ab511dd
thujeevan <thujee@gmail.com>
2016-06-10 15:13:09 +0530

    integrate react-redux's provider, routes

--- thujeevan/remo/containers/Root.js
+++ thujeevan/remo/containers/Root.js
@@ -1,14 +1,14 @@
 import React, { Component, PropTypes } from 'react'
-import { Provider } from 'react-redux'
-import routes from '../routes'
-import { Router } from 'react-router'
+import { Provider } from 'react-redux';
+import routes from '../routes';
+import { Router } from 'react-router';
 
 export default class Root extends Component {
   render() {
     const {store, history} = this.props;
     return (
         <Provider store={store}>
-            <Router history={history} routes={routes} />
+            <Router history={history} routes={routes(store)} />
         </Provider>
     );
   }
