9ee4271f2981160c5e7cda220a935db9393f55f1
thujeevan <thujee@gmail.com>
2016-06-13 16:48:22 +0530

    don't show dashboard unless authenticated

--- thujeevan/remo/routes.js
+++ thujeevan/remo/routes.js
@@ -6,12 +6,12 @@ import Login from './containers/LoginPage';
 import Dashboard from './components/Dashboard';
 
 export default function (store) {    
-    const connect = (fn) => (nextState, replace) => fn(store, nextState, replace);
+    const connect = (fn) => (nextState, replace) => fn && fn(store, nextState, replace);
     const onEnter = (Component) => connect(Component.onEnter);
     
     return (
         <Route path="/" component={App}>
-            <IndexRoute component={Dashboard} />
+            <IndexRoute component={Dashboard} onEnter={onEnter(Dashboard)}/>
             <Route path="/auth/login" component={Login} onEnter={onEnter(Login)}/>
         </Route>
     );
