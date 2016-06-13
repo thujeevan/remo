3073a2f1ed687d77e7e705f82562eea8f697d92d
thujeevan <thujee@gmail.com>
2016-06-13 13:24:20 +0530

    move auth reducer to a separate module

---
+++ thujeevan/remo/reducers/auth.js
@@ -0,0 +1,56 @@
+import {Map, List, fromJS} from 'immutable';
+import {loadData, SESSION_KEY} from '../storage/localStorage';
+
+import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_REQUEST, LOGOUT_SUCCESS} from '../actions';
+
+const session = loadData(SESSION_KEY);
+export const AUTH_INITIAL_STATE = fromJS({
+    isFetching: false, 
+    isAuthenticated: session && session.token ? true : false,
+    session
+});
+
+const authReducers = {
+    [LOGIN_REQUEST]: (state, {creds}) => {
+        return state.remove('session').remove('message').merge({
+            isFetching: true,
+            isAuthenticated: false,
+            creds
+        });
+    },
+    [LOGIN_SUCCESS]: (state, {session}) => {
+        return state.remove('creds').merge({
+            isFetching: false,
+            isAuthenticated: true,
+            session
+        });
+    },
+    [LOGIN_FAILURE]: (state, {message}) => {
+        return state.merge({
+            isFetching: false,
+            isAuthenticated: false,
+            message
+        });
+    },
+    [LOGOUT_REQUEST]: (state) => {
+        return state.merge({
+            isFetching: true
+        });
+    },
+    [LOGOUT_SUCCESS]: (state) => {
+        return state.merge({
+            isFetching: false,
+            isAuthenticated: false,
+            session: {}
+        });
+    },
+};
+
+export default function auth(state = AUTH_INITIAL_STATE, action) {
+    let reducer = authReducers[action.type] || (state => state);
+    return reducer(state, action);
+}
+
+export const getAuth = (state) => {
+    return state.toJS();
+}
--- thujeevan/remo/reducers/index.js
+++ thujeevan/remo/reducers/index.js
@@ -1,45 +1,7 @@
 import {Map, List, fromJS} from 'immutable';
 import {combineReducers} from 'redux';
-import {loadData} from '../storage/localStorage';
 
-import {LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT_REQUEST} from '../actions';
-
-// TODO: specify session key as a constant
-const session = loadData('remo-session');
-export const AUTH_INITIAL_STATE = fromJS({
-    isFetching: false, 
-    isAuthenticated: session && session.token ? true : false,
-    session
-});
-
-const authReducers = {
-    [LOGIN_REQUEST]: (state, {creds}) => {
-        return state.remove('session').remove('message').merge({
-            isFetching: true,
-            isAuthenticated: false,
-            creds
-        });
-    },
-    [LOGIN_SUCCESS]: (state, {session}) => {
-        return state.merge({
-            isFetching: false,
-            isAuthenticated: true,
-            session
-        });
-    },
-    [LOGIN_FAILURE]: (state, {message}) => {
-        return state.merge({
-            isFetching: false,
-            isAuthenticated: false,
-            message
-        });
-    }
-};
-
-function auth(state = AUTH_INITIAL_STATE, action) {    
-    let reducer = authReducers[action.type] || (state => state);
-    return reducer(state, action);
-}
+import auth from './auth';
 
 const rootReducer = combineReducers({auth});
 export default rootReducer;
