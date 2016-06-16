fb505a338b1ec2d58b75115a18c08d73469d1f0c
thujeevan <thujee@gmail.com>
2016-06-16 12:01:51 +0530

    include react-virtualized lib

--- thujeevan/remo/index.js
+++ thujeevan/remo/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {browserHistory} from 'react-router';
+import 'react-virtualized/styles.css';
 
 import configureStore from './store';
 import Root from './containers/Root';
