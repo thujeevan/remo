86d3047316b808e2d57331b2cd0e646b706efc07
thujeevan <thujee@gmail.com>
2016-06-16 14:39:53 +0530

    update UserList component to user virtual list

--- thujeevan/remo/components/UserList.js
+++ thujeevan/remo/components/UserList.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import VirtualList from './VirtualList';
 
-const UserList = (props) => {
+const UserList = (props) => {    
+    const rowRenderer = (user, isLoading, isScrolling) => {
+        const row = (            
+            <a href="#" className="list-group-item">
+                <i className="fa fa-user fa-3x pull-left"></i>
+                <h4 className="list-group-item-heading">
+                    {isLoading ? 'loading...' : user.get('full_name')}
+                </h4>
+                <p className="list-group-item-text">
+                    {isLoading ? 'loading...' : user.getIn(['primary_group', 'group_name'], 'Does not belong to any group')}
+                </p>
+            </a>
+        );
+        return row;
+    }
+    
     return (
-        <div> User List </div>
+        <div className="panel panel-default">
+            <div className="panel-heading">Users</div>
+            <div className="list-group">
+                <VirtualList {...props} rowRenderer={rowRenderer}/>
+            </div>
+        </div>        
     );
 };
 
