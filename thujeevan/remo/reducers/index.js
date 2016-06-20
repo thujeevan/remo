517d936a234409f9483601ef85ef292d99e3a1a1
thujeevan <thujee@gmail.com>
2016-06-20 15:46:51 +0530

    introduce shared entities subtree - separate entities reducer - will hold the id/uid mapped objects fetched from remote (normalized, deduped) - currently users, groups

--- thujeevan/remo/reducers/index.js
+++ thujeevan/remo/reducers/index.js
@@ -4,5 +4,17 @@ import {combineReducers} from 'redux';
 import auth from './auth';
 import users from './users';
 
-const rootReducer = combineReducers({auth, users});
+// shared entity namespace
+// will hold all entity objects
+export const ENTITIES_INITIAL_STATE = fromJS({users: {}, groups: {}});
+
+function entities(state = ENTITIES_INITIAL_STATE, action) {
+    if (action.res && action.res.entities) {
+        // currently everything get merged
+        return state.mergeDeep(action.res.entities);
+    }
+    return state;
+}
+
+const rootReducer = combineReducers({auth, users, entities});
 export default rootReducer;
