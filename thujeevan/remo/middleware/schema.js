30f1ae63c38234b87832f928ea2095457f7e0575
thujeevan <thujee@gmail.com>
2016-06-20 10:56:30 +0530

    add schema for user entity

--- thujeevan/remo/middleware/schema.js
+++ thujeevan/remo/middleware/schema.js
@@ -1,4 +1,5 @@
 import {Schema, arrayOf} from 'normalizr';
+import isEqual from 'lodash/isEqual';
 
 const sessionSchema = new Schema('sessions', {
     idAttribute: session => sesssion.key
@@ -13,8 +14,15 @@ const groupSchema = new Schema('groups', {
 });
 
 sessionSchema.define({
-    permissions: arrayOf(permissionSchema),
-    primary_group: groupSchema
+    permissions: arrayOf(permissionSchema)
+});
+
+const userSchema = new Schema('users', {
+    idAttribute: user => user.uid
+});
+userSchema.define({
+    primary_group: groupSchema,
+    groups: arrayOf(groupSchema)
 });
 
 export const Schemas = {
@@ -22,5 +30,27 @@ export const Schemas = {
     PERMISSION: permissionSchema,
     PERMISSION_ARRAY: arrayOf(permissionSchema),
     GROUP: groupSchema,
-    GROUP_ARRAY: arrayOf(groupSchema)
+    GROUP_ARRAY: arrayOf(groupSchema),
+    USER_SCHEMA: {
+        users: arrayOf(userSchema)
+    }
 };
+
+export const options = {
+    USER_SCHEMA: {
+        mergeIntoEntity: (entityA, entityB, entityKey) => {
+            if (entityKey == 'user_group') return;
+            
+            for (let key in entityB) {
+                if (!entityB.hasOwnProperty(key)) {
+                    continue;
+                }
+
+                if (!entityA.hasOwnProperty(key) || isEqual(entityA[key], entityB[key])) {
+                    entityA[key] = entityB[key];
+                    continue;
+                }
+            }
+        }
+    }
+}
