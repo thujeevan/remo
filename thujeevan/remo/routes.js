7354493f8ecf04e40d4f2b84dd2d996f46dc6a24
thujeevan <thujee@gmail.com>
2016-06-28 13:24:42 +0530

    add route - user profile

--- thujeevan/remo/routes.js
+++ thujeevan/remo/routes.js
@@ -5,6 +5,7 @@ import App from './containers/App';
 import Login from './containers/LoginPage';
 import Dashboard from './components/Dashboard';
 import Users from './containers/Users';
+import User from './containers/User';
 
 export default function (store) {    
     const connect = (fn) => (nextState, replace) => fn && fn(store, nextState, replace);
@@ -13,7 +14,9 @@ export default function (store) {
     return (
         <Route path="/" component={App}>
             <IndexRoute component={Dashboard} onEnter={onEnter(Dashboard)}/>
-            <Route path="/users" component={Users} onEnter={onEnter(Users)}/>
+            <Route path="/users" component={Users} onEnter={onEnter(Users)}>
+                <Route path=":uid" component={User}/>
+            </Route>
             <Route path="/auth/login" component={Login} onEnter={onEnter(Login)}/>
         </Route>
     );
