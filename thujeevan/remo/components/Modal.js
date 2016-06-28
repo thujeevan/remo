8e97c685f639eada4d663de349d5a6e2ed013a49
thujeevan <thujee@gmail.com>
2016-06-28 13:20:41 +0530

    add simple modal component

---
+++ thujeevan/remo/components/Modal.js
@@ -0,0 +1,28 @@
+import React, {PropTypes} from 'react';
+import BsModal from 'react-bootstrap/lib/Modal';
+
+const Modal = (props) => {
+    const {title, body, footer, ...other} = props;
+    const modal = (
+        <BsModal {...other}>
+            <BsModal.Header>
+                <BsModal.Title>{title}</BsModal.Title>
+            </BsModal.Header>
+            <BsModal.Body>
+                {body}
+            </BsModal.Body>
+            <BsModal.Footer>
+                {footer}
+            </BsModal.Footer>
+        </BsModal>
+    );
+    return modal;
+};
+
+Modal.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.element.isRequired,
+    footer: PropTypes.element.isRequired
+};
+
+export default Modal;
