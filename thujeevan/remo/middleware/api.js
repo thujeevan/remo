45cdaf661335c12a73e84ae7195b3e5de8222013
thujeevan <thujee@gmail.com>
2016-07-04 16:17:18 +0530

    handle HTTP methods - API middleware

--- thujeevan/remo/middleware/api.js
+++ thujeevan/remo/middleware/api.js
@@ -3,15 +3,30 @@ import 'isomorphic-fetch';
 
 import {SESSION_KEY, loadData} from '../storage/localStorage';
 
+export const METHOD_GET = 'GET';
+export const METHOD_POST = 'POST';
+export const METHOD_PUT = 'PUT';
+
 export const API_ROOT = "http://localhost:3000";
 
-function callApi(endpoint, schema, options = {}) {
+const configureFetch = (endpoint, body, method = METHOD_GET) => (schema, options) => {
     const fullURL = (endpoint.indexOf(API_ROOT) === -1) ? `${API_ROOT}${endpoint}` : endpoint;
     const {token} = loadData(SESSION_KEY);
     
+    body = typeof body === 'string' ? body : JSON.stringify(body);
+    
     const config = {
-        headers: { Authorization: token }
+        method,
+        headers: { 
+            "Content-Type": "application/json",
+            Authorization: token 
+        }
     };
+    
+    // Request with GET/HEAD method cannot have body
+    if (method !== METHOD_GET) {
+        config.body = body;
+    }
     
     return fetch(fullURL, config).then(res => res.json().then(json => ({json, res}))).then(({json, res}) => {
         if (!res.ok) {
@@ -20,7 +35,11 @@ function callApi(endpoint, schema, options = {}) {
         const normalized = Object.assign({}, normalize(json, schema, options));
         return normalized;
     });
-};
+}
+
+function callApi(fetchAPI, schema, options = {}) {
+    return fetchAPI(schema, options);
+}
 
 export const CALL_API = Symbol('Call API');
 
@@ -31,7 +50,7 @@ export default store => next => action => {
     }
     
     let {endpoint} = callAPI;
-    const {schema, options, types} = callAPI;
+    const {method, schema, options, types} = callAPI;
     
     if (typeof endpoint === 'function') {
         endpoint = endpoint(store.getState());
@@ -59,10 +78,11 @@ export default store => next => action => {
         return finalAction;
     }
     
+    const fetchAPI = configureFetch(endpoint, actionWith({}), method);    
     const [requestType, successType, failureType] = types;
-    next(actionWith({type: requestType}));
     
-    return callApi(endpoint, schema, options).then(
+    next(actionWith({type: requestType}));    
+    return callApi(fetchAPI, schema, options).then(
         res => next(actionWith({res, type: successType})), 
         error => next(actionWith({error, type: failureType}))
     );
