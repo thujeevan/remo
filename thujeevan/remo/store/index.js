4df5a5f43d7c5e7736afefaf0af691112b2a5cd2
thujeevan <thujee@gmail.com>
2016-07-06 11:43:56 +0530

    add HMR support for reducers

--- thujeevan/remo/store/index.js
+++ thujeevan/remo/store/index.js
@@ -5,8 +5,18 @@ import api from '../middleware/api';
 import rootReducer from '../reducers';
 
 export default function configureStore(preloadedState) {
-    return createStore(rootReducer, preloadedState, compose(
+    const store = createStore(rootReducer, preloadedState, compose(
         applyMiddleware(thunk, api),
         window.devToolsExtension ? window.devToolsExtension() : f => f
     ));
+    
+    if (module.hot) {
+        // Enable Webpack hot module replacement for reducers
+        module.hot.accept('../reducers', () => {
+            const nextRootReducer = require('../reducers').default
+            store.replaceReducer(nextRootReducer)
+        })
+    }
+    
+    return store;
 }
