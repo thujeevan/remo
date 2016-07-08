4f61dfcb94bd60218edd95cdcb8a6c46caf28310
thujeevan <thujee@gmail.com>
2016-07-08 13:12:42 +0530

    include alerts into app

--- thujeevan/remo/containers/App.js
+++ thujeevan/remo/containers/App.js
@@ -1,10 +1,12 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
+import React from 'react';
+
 import NavBar from '../containers/NavBar';
+import Alert from '../containers/Alert';
 
 const App = ({children}) => {
     return (
         <div>
+            <Alert />
             <NavBar />
             <div className="container-fluid">{children}</div>
         </div>
